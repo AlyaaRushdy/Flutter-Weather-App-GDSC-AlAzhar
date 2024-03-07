2fe5c8dad13773332883971022a089dd9f4ac750
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-03-08 00:05:03 +0200

    home page complete

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screens/search_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber.shade100,
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SearchPage(),
+                ),
+              );
+            },
+            icon: const Icon(
+              Icons.search_rounded,
+              size: 26,
+            ),
+          ),
+        ],
+        title: const Text("Weather App"),
+        backgroundColor: Colors.blue.shade700,
+        foregroundColor: Colors.white,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Text(
+            "There's No Result 😔 Try Searching From The Above Search Icon 🔍",
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+      ),
     );
   }
 }
