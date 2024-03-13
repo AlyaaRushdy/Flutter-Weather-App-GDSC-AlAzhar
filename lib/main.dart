32937e633d84f9b49a4c25e5e8a84584f917b441
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-03-13 09:57:08 +0200

    blocProvider added and theme changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubit/weather_state_cubit.dart';
 import 'package:weather_app/screens/home_page.dart';
+import 'package:weather_app/theme/get_theme_color.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,16 +14,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue.shade700,
-          primary: Colors.blue.shade700,
-          brightness: Brightness.light,
-        ),
+    return BlocProvider(
+      create: (context) => GetWeatherCubit(),
+      child: BlocBuilder<GetWeatherCubit, WeatherStates>(
+        builder: (context, state) {
+          return MaterialApp(
+            theme: ThemeData(
+              appBarTheme: AppBarTheme(
+                color: getThemeColor(BlocProvider.of<GetWeatherCubit>(context)
+                    .weatherModel
+                    ?.condition),
+                elevation: 1,
+              ),
+              primaryColor: getThemeColor(
+                  BlocProvider.of<GetWeatherCubit>(context)
+                      .weatherModel
+                      ?.condition),
+            ),
+            debugShowCheckedModeBanner: false,
+            home: const HomePage(),
+          );
+        },
       ),
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
     );
   }
 }
